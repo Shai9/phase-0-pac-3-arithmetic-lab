f316e151072678bb6e8d3eed337e2c7ac290d7a5
Dennis Shakava <dennis.shakava@student.moringaschool.com>
2022-06-05 22:21:02 +0300

    commit tree c6baec9b787bef997772042304e5274c3ed349f9

---
+++ index.js
@@ -0,0 +1,35 @@
+function add() {
+    // we'll fill this in shortly
+}
+function subtract() {
+    // we'll fill this in shortly
+}
+
+function multiply() {
+    // we'll fill this in shortly
+}
+function divide() {
+    // we'll fill this in shortly
+}
+
+function add() {
+    return "something" ;
+}
+
+function add(a, b){
+    return (a+b);
+
+}
+
+function add(a, b) {
+    return .toEqual 1078; 
+  }
+
+
+
+
+
+
+
+
+
